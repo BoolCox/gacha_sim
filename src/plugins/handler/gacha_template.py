da32674f02fe8dc83d9f2be308f901e521a613de
票票 <634823896@qq.com>
2026-04-21 15:09:30 +0800

    添加模板创建时的完整性检查，处理已存在模板的情况

--- src/plugins/handler/gacha_template.py
+++ src/plugins/handler/gacha_template.py
@@ -3,6 +3,7 @@ from nonebot.internal.params import ArgPlainText
 from nonebot_plugin_alconna import on_alconna, Match
 from nonebot_plugin_orm import async_scoped_session
 from sqlalchemy import select, delete
+from sqlalchemy.exc import IntegrityError
 
 from ..dependency.permission import ADMIN_PERMISSION
 from ..model.gacha_template import GachaTemplate, GachaRarity
@@ -65,14 +66,20 @@ async def create_template_got(
     if total != 100:
         await create_template.finish(f"所有稀有度概率之和须为 100，当前为 {total}")
 
-    template = GachaTemplate(name=name.result, description=description.result)
-    session.add(template)
-    await session.flush()  # 获取 template.id
+    try:
+        template = GachaTemplate(name=name.result, description=description.result)
+        session.add(template)
+        await session.flush()
 
-    for rarity_name, weight in rarity_entries:
-        session.add(GachaRarity(template_id=template.id, name=rarity_name, weight=weight))
+        for rarity_name, weight in rarity_entries:
+            session.add(GachaRarity(template_id=template.id, name=rarity_name, weight=weight))
 
-    await session.commit()
+        await session.commit()
+
+    except IntegrityError:
+        await session.rollback()
+        await create_template.finish(f"模板「{name.result}」已存在")
+
     lines = "\n".join(f"  {r}：{w}%" for r, w in rarity_entries)
     await create_template.finish(f"模板「{name.result}」创建成功\n{lines}")
 
--- src/plugins/model/gacha_account.py
+++
@@ -1,1 +0,0 @@
-"""Deprecated: gacha account model removed."""
